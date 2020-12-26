0a982910a834577c563341ac5a1d1f88ddf124bf
boy12371 <boy12371@hotmail.com>
2020-12-26 18:02:36 +0800

    Updates

---
+++ static/js/53.c4ea7304.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpionic-react-conference-app"]=this["webpackJsonpionic-react-conference-app"]||[]).push([[53],{149:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return o}));var c=e(16),o=function(){var n=window;n.addEventListener("statusTap",(function(){Object(c.h)((function(){var t=n.innerWidth,e=n.innerHeight,o=document.elementFromPoint(t/2,e/2);if(o){var i=o.closest("ion-content");i&&i.componentOnReady().then((function(){Object(c.f)((function(){return i.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.c4ea7304.chunk.js.map
